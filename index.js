d2d96f7568114d12136b2b17d7f1dcbec6a36091
Vic Huang <vic910812@gmail.com>
2021-07-05 15:55:16 +0800

    點擊顯示特定影片資訊

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function renderMovieList(data) {
     data.forEach((item) => {
         console.log(item)
         rawHTML +=
-        `<div class="col-sm-3">
+            `<div class="col-sm-3">
         <div class="mb-2">
             <div class="card">
                 <img class="card-img-top"
@@ -23,7 +23,7 @@ function renderMovieList(data) {
                 </div>
                 <div class="card-footer">
                   <button class="btn btn-primary btn-show-movie" data-toggle="modal"
-                      data-target="#movie-modal">More</button>
+                      data-target="#movie-modal" data-id="${item.id}">More</button>
                   <button class="btn btn-info btn-add-favorite">+</button>
                 </div>
             </div>
@@ -33,6 +33,32 @@ function renderMovieList(data) {
 
     dataPanel.innerHTML = rawHTML
 }
+
+function showMovieModel(id) {
+    const modalTitle = document.querySelector('#movie-modal-title')
+    const modalImage = document.querySelector('#movie-modal-image')
+    const modalDate = document.querySelector('#movie-modal-date')
+    const modalDescription = document.querySelector('#movie-modal-description')
+
+    axios.get(INDEX_URL + id).then(response => {
+        const data = response.data.results
+        modalTitle.innerText = data.title
+        modalDate.innerText = 'Release date:' + data.release_date
+        modalDescription.innerText = data.description
+        modalImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="image-fluid">`
+    })
+}
+
+//匿名函式寫法dataPanel.addEventListener('click', (event) => {
+//    console.error('Error')
+//    })
+//用下方給FUNCTION命名比較好，在DEBUG時會抓得出哪個函式錯誤，反之匿名函式只會說有匿名函式錯誤
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+    if (event.target.matches('.btn-show-movie')) {
+        console.log(event.target.dataset)
+        showMovieModel(Number(event.target.dataset.id))
+    }
+})
 
 axios.get(INDEX_URL).then((response) => {
     //console.log(response.data.results)
